84bf3ec006f8bccb532eb70386a0b35c7544687c
panweizeng <panweizeng@gmail.com>
2011-03-30 11:46:37 +0800

    update dict and proxy script

--- code/python/dict/dict.py
+++ code/python/dict/dict.py
@@ -5,14 +5,16 @@ import sys
 import re
 import xml.dom.minidom as xml
 
-API_URL = 'http://dict.cn/ws.php?utf8=true&q=%s'
-#API_URL = 'http://dict-co.iciba.com/api/dictionary.php?w=%s'
+#API_URL = 'http://dict.cn/ws.php?utf8=true&q=%s'
+API_URL = 'http://dict-co.iciba.com/api/dictionary.php?w=%s'
 def getword(word):
     xmls = urllib.urlopen(API_URL%urllib.quote(word)).read()
+    #print xmls
     root = xml.parseString(xmls).documentElement
     #print re.sub(u'>', '>\n',xmls)
 
-    tags = {'key':'单词', 'pron':'音标', 'def':'释义', 'sent':'例句', 'orig':'例句', 'trans':'翻译', 'acceptation':'释义'}
+    #tags = {'key':'单词', 'pron':'音标', 'def':'释义', 'sent':'例句', 'orig':'例句', 'trans':'翻译', 'acceptation':'释义'}
+    tags = {'key':'单词', 'ps':'音标', 'def':'释义', 'sent':'例句', 'orig':'例句', 'trans':'翻译', 'acceptation':'释义'}
 
     def isElement(node):
         return node.nodeType == node.ELEMENT_NODE
@@ -22,10 +24,10 @@ def getword(word):
         if isText(node):
             tag = tags.get(tagName, tagName)
             print '%s:%s'%(tag, node.nodeValue)
-        elif isElement(node):
+        elif isElement(node) and tags.has_key(node.tagName):
             [show(i, node.tagName) for i in node.childNodes]
 
-    [ show(i) for i in root.childNodes if isElement(i) and i.tagName in ['key', 'pron', 'def', 'sent', 'acceptation'] ]
+    [ show(i) for i in root.childNodes if isElement(i) ]
     
 def main():
     if len(sys.argv) >= 2:
